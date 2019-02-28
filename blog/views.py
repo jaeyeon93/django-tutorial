0fa78b5091b73f5ed155be65e262c573d1870f1e
JaeyeonKIM <foodsksms@gmail.com>
2019-03-01 01:25:20 +0900

    Template까지 학습

--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,13 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
-# Create your views here.
+def post_list(request):
+    qs = Post.objects.all()
+    qs = qs.filter(published_date__lte=timezone.now())
+    qs = qs.order_by('published_date')
+    return render(request, 'blog/post_list.html', {
+        'post_list' : qs
+    })
